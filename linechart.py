99cd8cd04821a714fc620d4a3b2d8d479eabe606
PCRDO\walls <wallstreetdoggo@gmail.com>
2024-01-31 19:44:08 +0100

    script purpose

--- linechart.py
+++ linechart.py
@@ -1,16 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#rndDataX = np.random.randint(100)
+#prices = np.random.randint(50, 100, size=14) --> aux func for integer arrays
 
 years = [2010 + x for x in range(14)]
 prices = np.random.random(14) * 50 + 50
-#prices = np.random.randint(50, 100, size=14)
 
+years2 = [2010 + x for x in range(14)]
+prices2 = np.random.random(14) * 50 + 50
+
+# plot charts --> linear representation, commonly used to show tendencies across time
 plt.plot(years, prices, "b--", lw=2)
+plt.plot(years2, prices2, "r-", lw=1)
 
-years = [2010 + x for x in range(14)]
-prices = np.random.random(14) * 50 + 50
-
-plt.plot(years, prices, "r-", lw=1)
 plt.show()
--- plttest.py
+++ plttest.py
@@ -1,14 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#rndDataX = np.random.randint(100)
+#rndDataX = np.random.randint(100) --> Auxiliar func for integer generation
 
-# (real numbers simulate price)
+# (real numbers to simulate price)
 rndDataX = np.random.random(500) * 120
 rndDataY = np.random.random(500) * 120
 
 #print(rndDataX)
 color = "#0DABCD"
 
+# Scatter chart --> standard distribution of values (that normaly do not hold a direct relation)
 plt.scatter(rndDataX, rndDataY, c=color, marker="x", alpha=0.5)
 plt.show()
